4d9307a5d8548913cd1503e13ef3eab68cddac5b
Mengtan <mengtan@startupinstitute.com>
2014-03-16 18:55:56 -0400

    heroku_mt

--- local_settings.py
+++ local_settings.py
@@ -5,7 +5,7 @@ Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 '''
 
 # Uncommet to configure in file.
-ACCOUNT_SID = "AC63cd1cf15913fcef3b77d83f29b95303" 
+ACCOUNT_SID = "AC63cd1cf15913fcef3b77d83f29b95303"
 AUTH_TOKEN = "e39b5e3330543a91e0d5265c85650dd3"
 BSSSPAM_APP_SID = "AP8f878da6ab66e40d36a3121d4b46aa47"
 BSS_SPAM_ID = "+16175051472"
